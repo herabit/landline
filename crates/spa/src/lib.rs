e874707bad76d83c2ee633845f72d5e0d3121b3c
= <hera@hera.wtf>
2026-09-10 18:11:45 -0700

    Restrict supported platforms further
    Just realized that there's a small possibility there's a mismatch in what we consider a `float` or `double`, and what the target platform does. We cause a compiler error upon a mismatch.

--- crates/spa/src/lib.rs
+++ crates/spa/src/lib.rs
@@ -1,12 +1,33 @@
 pub mod mem;
 pub mod pod;
 
+// Pipewire seems to be built with the assumption that we're on 32-bit or 64-bit linux,
+// and we will also make that assumption.
 #[allow(unexpected_cfgs)]
 const _: () = {
     cfg_select! {
-        any(target_pointer_width = "32", target_pointer_width = "64") => {}
+        all(
+            target_os = "linux",
+            any(target_pointer_width = "32", target_pointer_width = "64"),
+        ) => {}
         _ => {
-            compiler_error!("we only support 32-bit and 64-bit platforms currently");
+            compiler_error!("we only support 32-bit and 64-bit linux currently");
         }
     }
+};
+
+// Pipewire *also* seems to assume that `c_float == f32` and `c_double == f64`.
+//
+// We will assume as such here too. This will cause a compiler error if there's a mismatch in what
+// either are considered for our target platform.
+#[allow(clippy::let_unit_value)]
+const _: () = {
+    const fn do_nothing<T>(_: T)
+    where
+        T: Copy,
+    {
+    }
+
+    let _ = do_nothing::<std::ffi::c_float>(0.0_f32);
+    let _ = do_nothing::<std::ffi::c_double>(0.0_f64);
 };
